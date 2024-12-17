a82c4f4355b774480e3ed28a75b3cd6294078f6a
Victor Loures <victorloures10@gmail.com>
2024-12-16 21:27:28 -0300

    Criando util de JSON e exceptions

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/utils/JsonUtil.java
@@ -0,0 +1,30 @@
+package br.com.microservices.orchestrated.inventoryservice.core.utils;
+
+import org.springframework.stereotype.Component;
+
+import br.com.microservices.orchestrated.inventoryservice.core.dto.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(final Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (final Exception exception) {
+            return "";
+        }
+    }
+
+    public Event toEvent(final String json) {
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (final Exception exception) {
+            return null;
+        }
+    }
+}
